1a8d0a67dca64051c10209edb65c43e6504f4459
kondanarayana <narayanakonda3@gmail.com>
2018-12-06 20:14:56 +0530

    test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    fulfillmentText: req.body,
+    fulfillmentText: req.body.result.parameters.echoText,
     source: "webhook-echo-sample"
   });
 });
