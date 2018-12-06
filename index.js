4a7c3daccba658c3a7a3d9877ed66e9af80a13e9
kondanarayana <narayanakonda3@gmail.com>
2018-12-06 20:15:43 +0530

    adde log

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
+  console.log("req", req);
   var speech =
     req.body.result &&
     req.body.result.parameters &&
